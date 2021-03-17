860fb795008fb555047da22854a7f8d76eabf1ab
Rosmari <rosmarifernandez@hotmail.com>
2021-03-17 14:22:23 -0300

    listar noticias falta

--- TrabajoFinal/apps/categorias/forms.py
+++ TrabajoFinal/apps/categorias/forms.py
@@ -4,4 +4,4 @@ from .models import Redacción
 class Formulario_Alta_Redacción(forms.ModelForm):
     class Meta:
         model = Redacción
-        fields = ['titulo', 'texto', 'categoria', 'imagen']
+        fields = ['titulo', 'texto']
--- TrabajoFinal/apps/categorias/models.py
+++ TrabajoFinal/apps/categorias/models.py
@@ -2,22 +2,22 @@ from django.db import models
 
 
 # Create your models here.
-categorias_status=[
-    (1, 'Internacionales'),
-    (2, 'Nacionales'),
-    (3, 'Policiales'),
-    (4, 'Deportes'),
-]
+#categorias_status=[
+#    (1, 'Internacionales'),
+#    (2, 'Nacionales'),
+#    (3, 'Policiales'),
+#    (4, 'Deportes'),
+#]
 
 class Redacción(models.Model):
     titulo = models.CharField(max_length=100)
     texto = models.TextField() 
-    fecha = models.DateField(auto_now_add=True)
-    categoria = models.IntegerField(
-        null=False, blank=False,
-        choices=categorias_status
-    )
-    imagen = models.ImageField(upload_to='categorias')
+    fecha = models.DateTimeField(auto_now_add=True)
+  #  categoria = models.IntegerField(
+  #      null=False, blank=False,
+  #      choices=categorias_status
+  #  )
+#    imagen = models.ImageField()
 
     def __str__(self):
         return (self.nombre)
