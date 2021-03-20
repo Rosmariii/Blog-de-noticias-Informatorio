53eb533113ace46aa693dc1dc9851e87342d3f2a
Rosmari <rosmarifernandez@hotmail.com>
2021-03-20 15:58:25 -0300

    imagen

--- TrabajoFinal/apps/categorias/forms.py
+++ TrabajoFinal/apps/categorias/forms.py
@@ -4,4 +4,4 @@ from .models import Redaccion
 class Formulario_Alta_Redaccion(forms.ModelForm):
     class Meta:
         model = Redaccion
-        fields = ['titulo', 'texto', 'fecha']
+        fields = ['titulo', 'texto', 'fecha', 'categoria', 'imagen']
--- TrabajoFinal/apps/categorias/models.py
+++ TrabajoFinal/apps/categorias/models.py
@@ -1,23 +1,23 @@
 from django.db import models
+from django.db.models.fields import DateField, TimeField
 
 
 # Create your models here.
-#categorias_status=[
-#    (1, 'Internacionales'),
-#    (2, 'Nacionales'),
-#    (3, 'Policiales'),
-#    (4, 'Deportes'),
-#]
+categorias_status=[
+    (1, 'Internacionales'),
+    (2, 'Nacionales'),
+    (3, 'Policiales'),
+    (4, 'Deportes'),
+]
 
 class Redaccion(models.Model):
     titulo = models.CharField(max_length=100)
     texto = models.TextField() 
-    fecha = models.DateField(auto_now_add=False, auto_now=False, blank=False)
-  #  categoria = models.IntegerField(
-  #      null=False, blank=False,
-  #      choices=categorias_status
-  #  )
-#    imagen = models.ImageField()
+    fecha = models.DateField(auto_now_add=False, auto_now=False, blank=False, )
+    categoria = models.IntegerField(null=True, blank=True,
+        choices=categorias_status,
+    )
+    imagen = models.ImageField(upload_to='categorias', null=True, blank=True )
 
     def __str__(self):
         return (self.titulo)
