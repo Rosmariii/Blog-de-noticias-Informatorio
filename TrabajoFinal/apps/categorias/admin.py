29fb7d7a0c290ebb16af84cc70b60df4f5a946e9
Rosmari <rosmarifernandez@hotmail.com>
2021-03-21 02:58:36 -0300

    crud superusuario

--- TrabajoFinal/apps/categorias/admin.py
+++ TrabajoFinal/apps/categorias/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Redaccion, Category
 # Register your models here.
+admin.site.register(Redaccion)
+admin.site.register(Category)
--- TrabajoFinal/apps/categorias/models.py
+++ TrabajoFinal/apps/categorias/models.py
@@ -1,23 +1,32 @@
 from django.db import models
-from django.db.models.fields import DateField, TimeField
-
-
+from django.db.models.fields import CharField
+from django.utils import timezone
+from django.urls import reverse
 # Create your models here.
-categorias_status=[
-    (1, 'Internacionales'),
-    (2, 'Nacionales'),
-    (3, 'Policiales'),
-    (4, 'Deportes'),
-]
+class Category(models.Model):
+    nombre = CharField(max_length=50)
+
+    def __str__(self):
+        return (self.nombre)
+
+    def get_absolute_url(self):
+        return reverse('base')
+#categorias_status=[
+#    (1, 'Internacionales'),
+#    (2, 'Nacionales'),
+#    (3, 'Policiales'),
+#    (4, 'Deportes'),
+#]
 
 class Redaccion(models.Model):
     titulo = models.CharField(max_length=100)
     texto = models.TextField() 
-    fecha = models.DateField(auto_now_add=False, auto_now=False, blank=False, )
-    categoria = models.IntegerField(null=True, blank=True,
-        choices=categorias_status,
-    )
-    imagen = models.ImageField(upload_to='categorias', null=True, blank=True )
+    fecha = models.DateField(default=timezone.now)
+    categoria = models.CharField(max_length=255, default='coding')
+    #imagen = models.ImageField(verbose_name='Imagen', upload_to='categorias')
 
     def __str__(self):
         return (self.titulo)
+
+    def get_absolute_url(self):
+        return reverse('base')
